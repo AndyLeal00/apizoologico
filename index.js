f2275db6aa6269f44677f801a3b12c99549e8a3e
AndyLeal00 <46579163+AndyLeal00@users.noreply.github.com>
2026-03-12 19:52:57 -0500

    Avance clase

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-'use strict';
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Hola Mundo');
-});
-server.listen(5000);
+app.get('/queso', (req, res) => {
+    res.send('¡Hola Mundo!');
+})
+
+app.listen(port, () => {
+    console.log('La aplicación se está ejecutando por el puerto + `${port}`');
+})
